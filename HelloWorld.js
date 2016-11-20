f6c7714e592527792b4cd02be7672894f4d3f1f7
surendrakandira93 <surendrakandira@gmail.com>
2016-11-20 17:05:50 +0530

    upload type script example
    Signed-off-by: surendrakandira93 <surendrakandira@gmail.com>

---
+++ HelloWorld.js
@@ -0,0 +1,5 @@
+var p = document.createElement('p');
+var hello = "Hello";
+var world = "2";
+p.textContent = hello + " " + world;
+document.body.appendChild(p);
---
+++ HelloWorld.ts
@@ -0,0 +1,5 @@
+var p = document.createElement('p');
+var hello: string = "Hello";
+var world: string = "2";
+p.textContent = hello + " " + world;
+document.body.appendChild(p);
